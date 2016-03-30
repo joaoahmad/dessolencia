14a3fe4632826e65522c198e831c73c105261a23
Joao Ahmad <jopejp@gmail.com>
2016-03-30 19:12:53 -0300

    up

--- public/js/app/application.js
+++ public/js/app/application.js
@@ -3,4 +3,18 @@ import ReactDom from 'react-dom';
 import {Router, Route, browserHistory} from 'react-router';
 import routes from './routes';
 
+if ('serviceWorker' in navigator) {
+    console.log('Service Worker is supported');
+    navigator.serviceWorker.register('sw.js').then(function(reg) {
+        console.log(':^)', reg);
+        reg.pushManager.subscribe({
+            userVisibleOnly: true
+        }).then(function(sub) {
+            console.log('endpoint:', sub.endpoint);
+        });
+    }).catch(function(error) {
+        console.log(':^(', error);
+    });
+}
+
 ReactDom.render((<Router history={browserHistory}>{routes}</Router>), document.getElementById('app'));
